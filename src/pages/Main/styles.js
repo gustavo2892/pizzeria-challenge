0891c407de03cf777bc145dbb9ad0085e255044b
gustavo2892 <gustavo2892@gmail.com>
2020-11-24 09:49:56 -0300

    Base Project with eslint and prettier

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const Title = styled.h1`
+  font-size: 24px;
+  color: #fff;
+  font-family: Arial, Helvetica, sans-serif;
+
+  code {
+    color: #c8a2c8;
+  }
+`;
